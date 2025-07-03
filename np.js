59df85b22d363acede2b6f68042b5277bc021cb1
Igor Ponomarev <9129910001@mail.ru>
2025-07-03 15:27:48 +0500

    update

--- np.js
+++ np.js
@@ -7,6 +7,8 @@
   var BASE_URL = 'https://numparser.igorek1986.ru';
   var ICON = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g><path fill="currentColor" d="M482.909,67.2H29.091C13.05,67.2,0,80.25,0,96.291v319.418C0,431.75,13.05,444.8,29.091,444.8h453.818c16.041,0,29.091-13.05,29.091-29.091V96.291C512,80.25,498.95,67.2,482.909,67.2z M477.091,409.891H34.909V102.109h442.182V409.891z"/></g></g><g><g><rect fill="currentColor" x="126.836" y="84.655" width="34.909" height="342.109"/></g></g><g><g><rect fill="currentColor" x="350.255" y="84.655" width="34.909" height="342.109"/></g></g><g><g><rect fill="currentColor" x="367.709" y="184.145" width="126.836" height="34.909"/></g></g><g><g><rect fill="currentColor" x="17.455" y="184.145" width="126.836" height="34.909"/></g></g><g><g><rect fill="currentColor" x="367.709" y="292.364" width="126.836" height="34.909"/></g></g><g><g><rect fill="currentColor" x="17.455" y="292.364" width="126.836" height="34.909"/></g></g></svg>';
   var DEFAULT_MIN_PROGRESS = 90;
+  var MIN_PROGRESS = Lampa.Storage.get('numparser_min_progress', DEFAULT_MIN_PROGRESS);
+  var newProgress = MIN_PROGRESS;
 
   // Функция фильтрации просмотренных
   function filterWatchedContent(results) {
@@ -47,7 +49,7 @@
                   var view = Lampa.Storage.cache('file_view', 300, [])[hashes[i]];
                   if (view) {
                       hasProgress = true;
-                      if (!view.percent || view.percent >= DEFAULT_MIN_PROGRESS) {
+                      if (!view.percent || view.percent >= MIN_PROGRESS) {
                           return false;
                       }
                   }
@@ -140,7 +142,7 @@
           ].join(''));
           
           var view = Lampa.Timeline.view(hash);
-          return view.percent > DEFAULT_MIN_PROGRESS;
+          return view.percent > MIN_PROGRESS;
       });
   }
 
@@ -448,6 +450,7 @@
           loadPart(onSuccess, onError);
           return loadPart;
       };
+
       Lampa.Listener.follow('line', async function (event) {
           if (event.type !== 'append') return;
           var data = event.data;
@@ -487,8 +490,6 @@
           }
       });
   }
-
-
 
   function Episode(data) {
       var self = this;
@@ -628,7 +629,7 @@
               title: SOURCE_NAME
           };
       }
-
+      
       var values = Lampa.Params.values.start_page;
       values[SOURCE_NAME] = SOURCE_NAME;
 
@@ -656,6 +657,37 @@
           }
       });
 
+      // Добавляем настройку прогресса
+      Lampa.SettingsApi.addParam({
+          component: 'numparser_settings',
+          param: {
+              name: 'numparser_min_progress',
+              type: 'select',
+              values: {
+                  '50': '50%',
+                  '55': '55%',
+                  '60': '60%',
+                  '65': '65%',
+                  '70': '70%',
+                  '75': '75%',
+                  '80': '80%',
+                  '85': '85%',
+                  '90': '90%',
+                  '95': '95%',
+                  '100': '100%'
+              },
+              default: DEFAULT_MIN_PROGRESS.toString()
+          },
+          field: {
+              name: 'Порог просмотра',
+              description: 'Минимальный процент просмотра для скрытия контента'
+          },
+          onChange: function(value) {
+              newProgress = parseInt(value);
+              Lampa.Storage.set('numparser_min_progress', newProgress);
+              MIN_PROGRESS = newProgress;
+          }
+      });
 
       // Настройка для изменения названия источника
       Lampa.SettingsApi.addParam({
@@ -671,7 +703,6 @@
               name: 'Название источника',
               description: 'Изменение названия источника в меню'
           },
-
           onChange: function (value) {
               newName = value;
               $('.num_text').text(value);
