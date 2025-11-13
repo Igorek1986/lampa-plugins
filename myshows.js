281fac39e6d503d59b36f108c57d79c9c1908604
Igor Ponomarev <9129910001@mail.ru>
2025-11-13 15:19:40 +0500

    fixed the myshows plugin. Added the use of authorization via CUB

--- myshows.js
+++ myshows.js
@@ -13,7 +13,6 @@
     };
     var AUTHORIZATION = 'authorization2'
     var syncInProgress = false;
-    var originalTimelineListener = null; 
     var watch_icon = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" fill="currentColor"/></svg>';
     var later_icon = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" fill="currentColor"/></svg>';
     var remove_icon = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="currentColor"/></svg>';
@@ -23,7 +22,7 @@
     function accountUrl(url) {  
         url = url + '';  
         if (url.indexOf('uid=') == -1) {  
-            var uid = Lampa.Storage.get('lampac_unic_id', '');  
+            var uid = Lampa.Storage.get('account_email') || Lampa.Storage.get('lampac_unic_id');
             if (uid) url = Lampa.Utils.addUrlComponent(url, 'uid=' + encodeURIComponent(uid));  
         }  
         return url;  
